a96c15951bc2c99a71a8506f3c7769713f1effa3
khanguslee <khanguslee@gmail.com>
2022-04-22 14:32:56 +1000

    feat: Create basic page layout

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// API Documentation found in https://www.npmjs.com/package/@mongodb-js/charts-embed-dom
+import ChartsEmbedSDK from "@mongodb-js/charts-embed-dom";
+
+const sdk = new ChartsEmbedSDK({
+  baseUrl: "https://charts.mongodb.com/charts-project-0-uvgyb",
+});
+
+const dashboard = sdk.createDashboard({
+  dashboardId: "6252bcf4-70f4-400e-8a06-68f6a46e26d0",
+  showAttribution: false,
+  widthMode: "fixed",
+  heightMode: "scale",
+});
+
+async function renderDashboard() {
+  await dashboard.render(document.getElementById("dashboard"));
+}
+
+renderDashboard().catch((e) => window.alert(e.message));
